701ed132a8bcef72ab78cd34ddac7e7e1c11da88
shanehughes1990 <shanehughes03301990@gmail.com>
2023-01-05 12:07:13 -0500

    updating ddns

--- workspaces/ingress/ddns.tf
+++ workspaces/ingress/ddns.tf
@@ -3,62 +3,47 @@ module "cloudflare_ddns" {
   source     = "../../modules/deployment"
   name       = "cloudflare-ddns"
   namespace  = kubernetes_namespace_v1.namespace.metadata[0].name
+  image_url  = "cr.hotio.dev/hotio/cloudflareddns"
+  image_tag  = "latest"
 
-  containers = [
-    {
-      name      = "cloudflare-ddns"
-      image_url = "cr.hotio.dev/hotio/cloudflareddns"
-      image_tag = "latest"
-
-      env = [
-        {
-          name  = "PUID"
-          value = 1000
-        },
-        {
-          name  = "PGID"
-          value = 1000
-        },
-        {
-          name  = "TZ"
-          value = "America/Toronto"
-        },
-        {
-          name  = "UMASK"
-          value = 002
-        },
-        {
-          name  = "INTERVAL"
-          value = 300
-        },
-        {
-          name  = "DETECTION_MODE"
-          value = "dig-whoami.cloudflare"
-        },
-        {
-          name  = "LOG_LEVEL"
-          value = 2
-        },
-        {
-          name  = "CF_RECORDTYPES"
-          value = "A"
-        },
-      ]
-
-      env_secrets = [
-        {
-          name  = "CF_APITOKEN"
-          value = var.cloudflare_config.api_token
-        },
-        {
-          name  = "CF_HOSTS"
-          value = var.cloudflare_config.zone_name
-        },
-        {
-          name  = "CF_ZONES"
-          value = var.cloudflare_config.zone_name
-        },
-      ]
-    }
-  ]
+  env = setunion(
+    local.common_env,
+    [
+      {
+        name  = "UMASK"
+        value = 002
+      },
+      {
+        name  = "INTERVAL"
+        value = 300
+      },
+      {
+        name  = "DETECTION_MODE"
+        value = "dig-whoami.cloudflare"
+      },
+      {
+        name  = "LOG_LEVEL"
+        value = 2
+      },
+      {
+        name  = "CF_RECORDTYPES"
+        value = "A"
+      },
+      {
+        name      = "CF_APITOKEN"
+        value     = var.cloudflare_config.api_token
+        is_secret = true
+      },
+      {
+        name      = "CF_HOSTS"
+        value     = var.cloudflare_config.zone_name
+        is_secret = true
+      },
+      {
+        name      = "CF_ZONES"
+        value     = var.cloudflare_config.zone_name
+        is_secret = true
+      },
+    ]
+  )
 }
