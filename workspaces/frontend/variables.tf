a79ff3efb053f1625ced839a1681c44bdfc3771f
shanehughes1990 <shanehughes03301990@gmail.com>
2023-01-10 22:56:46 -0500

    adding vaultwarden

--- workspaces/frontend/variables.tf
+++ workspaces/frontend/variables.tf
@@ -20,3 +20,16 @@ variable "cloudflare_config" {
     zone_id   = string
   })
 }
+
+variable "vaultwarden_config" {
+  sensitive = true
+  type = object({
+    admin_token    = string
+    smtp_host      = string
+    smtp_port      = number
+    smtp_from_name = string
+    smtp_username  = string
+    smtp_password  = string
+    smtp_timeout   = optional(number, 15)
+  })
+}
