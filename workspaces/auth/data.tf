e718df4aed2887066669b3e753205a1a9757fe6d
Shane Hughes <shanehughes03301990@gmail.com>
2023-10-12 00:18:11 +0000

    removing auth workspace

--- workspaces/auth/data.tf
+++
@@ -1,9 +0,0 @@
-# data "terraform_remote_state" "shared" {
-#   backend = "kubernetes"
-
-#   config = {
-#     secret_suffix = "shared"
-#     namespace     = "kube-public"
-#     config_path   = "~/.kube/config"
-#   }
-# }
