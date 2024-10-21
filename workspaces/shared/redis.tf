944f620575f5e77aedab675c3ea7944b0810334b
Shane Hughes <shanehughes03301990@gmail.com>
2024-10-21 13:55:55 +0000

    adding a redis container

---
+++ workspaces/shared/redis.tf
@@ -0,0 +1,25 @@
+module "redis" {
+  depends_on           = [kubernetes_namespace_v1.namespace]
+  source               = "../../modules/deployment"
+  name                 = "redis"
+  namespace            = kubernetes_namespace_v1.namespace.metadata[0].name
+  image_url            = "bitnami/redis"
+  image_tag            = "latest"
+  metadata_annotations = local.keel_annotations
+
+  ports = [
+    {
+      name           = "app-port"
+      service_type   = "NodePort"
+      container_port = 6379
+      node_port      = 30007
+    }
+  ]
+
+  env = [
+    {
+      name  = "ALLOW_EMPTY_PASSWORD"
+      value = "yes"
+    },
+  ]
+}
