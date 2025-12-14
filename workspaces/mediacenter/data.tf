3144da0903e20bbf83d612966349e28eb5ae5665
Shane Hughes <shanehughes03301990@gmail.com>
2025-12-14 04:36:25 +0000

    Remove Traefik and associated configurations from ingress workspace; delete mediacenter workspace and shared resources including PostgreSQL, Redis, and Cloud SQL Proxy configurations; clean up variables and lock files across workspaces.

--- workspaces/mediacenter/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "frontend" {
-  backend = "kubernetes"
-
-  config = {
-    secret_suffix = "frontend"
-    namespace     = "kube-public"
-    config_path   = "~/.kube/config"
-  }
-}
--- workspaces/shared/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "postgres" {
-  sensitive = true
-  value = {
-    username = var.postgres_config.username
-    password = var.postgres_config.password
-    svc      = "postgres-sql-port.${kubernetes_namespace_v1.namespace.metadata[0].name}.svc"
-  }
-}
